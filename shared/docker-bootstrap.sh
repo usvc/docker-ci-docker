5678db27339efa91810489276b23d2e8edbfffa7
zephinzer <dev@joeir.net>
2019-08-17 17:44:15 +0800

    updated bootstrap script (was broken)

--- shared/docker-bootstrap.sh
+++ shared/docker-bootstrap.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
-apk add --no-cache docker py-pip python-dev libffi-dev libc-dev openssl-dev;
+set -x;
+
+#
+# base docker tools
+#
+apk add --no-cache docker gcc libffi-dev musl-dev openssl-dev py-pip python-dev;
 pip install --no-cache-dir docker-compose;
+apk del --no-cache gcc py-pip
+
+#
+# container-structure-test
+#
 curl -Lo /bin/container-structure-test "https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-amd64";
 chmod +x /bin/container-structure-test;
